2281bf99a77ac7c358dd1f8d3b7e269457a5d686
Moaz Ashraf <moaza2298@gmail.com>
2026-09-08 15:13:09 +0300

    feat: add dashboard login

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,11 +3,13 @@ import authRoutes from "../modules/auth/auth.route.js";
 import captainRoutes from "../modules/captain/captain.routes.js";
 import compoundBoundaryRoutes from "../modules/compound-boundary/compound-boundary.routes.js";
 import pricingRoutes from "../modules/pricing/pricing.routes.js";
+import adminAuthRoutes from "../modules/admin/auth/auth.route.js";
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/captains", captainRoutes);
 router.use("/compound-boundary", compoundBoundaryRoutes);
 router.use("/pricing", pricingRoutes);
+router.use("/dashboard", adminAuthRoutes);
 
 export default router;
